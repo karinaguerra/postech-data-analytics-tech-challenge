73e577f20a16618424d594648969fc70ac40ef4c
Karina Guerra <105876378+karinaguerra@users.noreply.github.com>
2024-07-11 21:00:42 -0300

    Add files via upload

--- fase_4/streamlit/util/load_data_limpo.py
+++ fase_4/streamlit/util/load_data_limpo.py
@@ -8,7 +8,7 @@ def load_data_limpo(file_path):
 
 def main():
     # Caminho para o arquivo CSV
-    file_path = "C:/Users/KarinaNascimento1/Documents/05_Pessoal/02_Fiap/FASE4/fase_4/streamlit/data/ipeadata_limpo.csv"
+    file_path = "data/ipeadata_limpo.csv"
 
     # Carregar os dados do arquivo CSV
     df = load_data_limpo(file_path)
